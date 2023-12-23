86bd7b07d8136956a15d203f7e979478bc6f28ae
codechefsong <133816663+codechefsong@users.noreply.github.com>
2023-12-23 16:48:40 -0500

    add ERC721 NFT

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -31,6 +31,12 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   //   autoMine: true,
   // });
 
+  await deploy("WalletNFT", {
+    from: deployer,
+    log: true,
+    autoMine: true,
+  });
+
   await deploy("ERC6551Registry", {
     from: deployer,
     log: true,
