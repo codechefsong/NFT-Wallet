8e2ae206877edb677cdd2abe8d71719a3f9fbf43
codechefsong <133816663+codechefsong@users.noreply.github.com>
2023-12-27 16:35:29 -0500

    add a marketplace page

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -33,23 +33,6 @@ const Home: NextPage = () => {
     contractAddress: "0xcba265A5D7B7B0F8b5c5a7bC44e0e214372991c6",
   });
 
-  const { writeAsync: createAccount } = useScaffoldContractWrite({
-    contractName: "ERC6551Registry",
-    functionName: "createAccount",
-    args: [
-      deployedContracts[CHAIN_ID].ERC6551Account.address,
-      BigInt("1"),
-      deployedContracts[CHAIN_ID].WalletNFT.address,
-      BigInt("1"),
-      BigInt("1"),
-      "0x",
-    ],
-    onBlockConfirmation: txnReceipt => {
-      console.log("📦 Transaction blockHash", txnReceipt.blockHash);
-      console.log(txnReceipt);
-    },
-  });
-
   const { writeAsync: withdraw } = useScaffoldContractWrite({
     contractName: "ERC6551Account",
     functionName: "execute",
@@ -67,12 +50,6 @@ const Home: NextPage = () => {
         <div className="px-5">
           <p>Token Bound Account: {tbaAddress}</p>
           <p>Balance: {tbaAddress && <Balance address={tbaAddress as Address} />}</p>
-          <button
-            className="py-2 px-16 mb-10 mt-3 bg-green-500 rounded baseline hover:bg-green-300 disabled:opacity-50"
-            onClick={() => createAccount()}
-          >
-            Create Token Bound Account
-          </button>
           <br />
           <button
             className="py-2 px-16 mb-10 mt-3 bg-green-500 rounded baseline hover:bg-green-300 disabled:opacity-50"
